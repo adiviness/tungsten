9569f78ac1e40c8a35b93776c69bdae9c0e92f24
Austin Diviness <austin.diviness@gmail.com>
2014-08-06 01:14:02 -0600

    added tests for parser match methods

---
+++ test/parser_test.py
@@ -0,0 +1,40 @@
+
+import unittest
+from parser.parser import *
+from parser.scanner import Scanner, TokenType, Token 
+from parser.nodes import *
+
+class TestParser(unittest.TestCase):
+
+    def setUp(self):
+        self.scanner = Scanner()
+        self.parser = Parser()
+
+    def test_no_input(self):
+        self.parser.parse([])
+        self.assertEqual(type(self.parser.root_node), BlockNode)
+        self.assertEqual(len(self.parser.root_node.children), 0)
+
+    def test_match_one_type(self):
+        self.parser.tokens = []
+        int_token = Token(TokenType.INTEGER, "3")
+        self.assertFalse(self.parser.match(TokenType.INTEGER))
+        self.parser.tokens.append(int_token)
+        self.assertTrue(self.parser.match(TokenType.INTEGER))
+
+    def test_match_multiple_types(self):
+        int_token = Token(TokenType.INTEGER, "3")
+        self.parser.tokens = [int_token]
+        self.assertTrue(self.parser.match(TokenType.STRING, TokenType.INTEGER))
+
+    def test_matchN(self):
+        self.parser.tokens = [Token(TokenType.STRING, "hello"),
+                              Token(TokenType.STRING, "world"),
+                              Token(TokenType.INTEGER, "7")]
+        self.assertFalse(self.parser.matchN(TokenType.INTEGER, 3))
+        self.assertTrue(self.parser.matchN(TokenType.STRING, 0))
+        self.assertTrue(self.parser.matchN(TokenType.INTEGER, 2))
+
+    
+if __name__ == '__main__':
+    unittest.main()
